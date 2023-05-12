090d869293a312b7ceeb405819a24b1e399103cb
Seob <78734659+mcar100@users.noreply.github.com>
2023-05-13 03:01:49 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,7 @@
+function solution(array, commands) {
+      const answer = commands.map(([i,j,k])=>{
+      const slicedArray = array.slice(i-1,j).sort((a,b)=>a-b);
+      return slicedArray[k-1];
+    })
+    return answer;
+}
