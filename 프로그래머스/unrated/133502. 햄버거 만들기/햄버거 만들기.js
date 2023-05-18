4ef3cf510b03f70657e7e0fbd0e3986cc9b55740
Seob <78734659+mcar100@users.noreply.github.com>
2023-05-18 21:11:40 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,19 @@
+function solution(ingredient) {
+    let count = 0
+    const bugger = []
+    
+    ingredient.forEach((i,idx)=>{
+        bugger.push(i);
+        if(bugger.length>=4){
+            const check = bugger.slice(-4).join('');  // 뒤에서 4개 자르고 자른 부분 리턴
+            if(check==='1231'){
+                bugger.splice(-4);                      // 뒤에서 4개 자른 자신을 리턴
+                count++;    
+            }
+        }
+        
+    })
+    
+    
+    return count;
+}
