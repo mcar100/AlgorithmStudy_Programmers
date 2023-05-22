3e84afd0d182180384125b030e31c3e5f0f281f5
Seob <78734659+mcar100@users.noreply.github.com>
2023-05-22 22:31:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,15 @@
+// s를 배열로 어떻게 쪼갤까..
+function solution(s) {
+    let answer = s.replace(/zero/g,0);
+       answer = answer.replace(/one/g,1); 
+    answer = answer.replace(/two/g,2);
+    answer = answer.replace(/three/g,3);
+    answer = answer.replace(/four/g,4);
+    answer = answer.replace(/five/g,5);
+    answer = answer.replace(/six/g,6);
+    answer = answer.replace(/seven/g,7);
+    answer = answer.replace(/eight/g,8);
+    answer = answer.replace(/nine/g,9);
+
+    return parseInt(answer);
+}
