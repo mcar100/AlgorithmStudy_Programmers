05479ab697ed6f0fa76c4ff2398fd7e5bd4b24c9
Seob <78734659+mcar100@users.noreply.github.com>
2023-05-24 21:45:17 +0900

    [level 2] Title: 숫자의 표현, Time: 4.12 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -8,20 +8,19 @@ function solution(n) {
     
     console.log(limit);
     while(start<limit){
-    for(let i=start; i<n; i++){
-        if(num>=n){
-            if(num===n){
-                count++;
+        for(let i=start; i<n; i++){
+            if(num>=n){
+                if(num===n){
+                    count++;
+                }
+                start++;
+                num=0;
+                break;
             }
-            start++;
-            num=0;
-            break;
-        }
-        else
-        {
-            num += i;
-        }
-     }        
+            else{
+                num += i;
+            }
+        }        
     }
     return count+1;
 }
