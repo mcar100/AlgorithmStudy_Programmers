3b3552b7625a9670768b8c0c5e2c135629c8acbb
Seob <78734659+mcar100@users.noreply.github.com>
2023-08-28 17:21:35 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.92 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,9 +1,11 @@
+// 문자열을 쪼개 2차원 배열로 만드는 함수
 function make2DimensionArray(array){
     const newArray = [];
     array.forEach(e=>newArray.push(e.split(' ')));
     return newArray;
 }
 
+// 두 날짜를 비교하는 함수
 function compareTodayTerms(today, privacy, term){
     const todayDate = new Date(today);
     const privacyDate = new Date (privacy);
@@ -14,10 +16,12 @@ function compareTodayTerms(today, privacy, term){
 function solution(today, terms, privacies) {
     const answer = [];
     const termsArr = make2DimensionArray(terms);
+    // 2차원 배열 객체로 만들기
     const termsObj = termsArr.reduce((acc,cur)=>{
         return { ...acc ,  [cur[0]] : cur[1] };
     },{})
     const privaciesArr = make2DimensionArray(privacies);
+    
     privaciesArr.forEach((privacy,idx)=>{
         const compare = compareTodayTerms(today,privacy[0],termsObj[privacy[1]]);
         if(compare){
