83797922a1d673f299c10ec9e2f3c65620554aa3
Seob <78734659+mcar100@users.noreply.github.com>
2023-10-10 16:58:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 2.38 ms
+메모리: 33.6 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 16:42:27
+2023년 10월 2일 16:58:46
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,36 +1,42 @@
+// 괄호 맵
 const PARENTHESES = {
     '[': ']',
     '(': ')',
     '{': '}'
 }
 
+// 왼쪽으로 이동한 문자열 반환하는 함수
 function moveToLeft(str,idx){
-    const sliced1 = str.slice(0,idx);
-    const sliced2 = str.slice(idx,str.length);
-    const combinedString = sliced2.concat(sliced1);
+    const combinedString = str.slice(idx) + str.slice(0,idx);
     return combinedString;
 }
 
+// 올바른 괄호를 확인하는 함수
 function checkCorrectString(str){
     let check = [];
     const regex = /^[\(\{\[]/;
+    
     for(let i=0; i<str.length; i++){
+        // 여는 괄호 스택에 추가
         if(regex.test(str[i])){
             check.push(str[i]);
         }
+        // 닫는 괄호면 스택에서 pop
         else{
+            // 스택이 비어있을 시 false
             if(check.length===0){
                 return false;
             }
             
             const popStr = check.pop();
+            // pop한 괄호의 닫힌 괄호와 문자열이 다르면 false
             if(str[i] !== PARENTHESES[popStr]){
-                console.log(str[i], popStr);
                 return false;
             }
         }
     }
 
+    // 스택이 완전히 비워지면 true
     if(check.length===0){
         return true;
     }
@@ -41,8 +47,14 @@ function checkCorrectString(str){
 function solution(s) {
     let answer = 0;
     
+    // 길이가 홀수면 짝이 안맞으므로 0 반환 
+    if(s.length%2===1){
+        return 0;
+    }
+    
     for(let i=0; i<s.length; i++){
         const string = moveToLeft(s,i);
+        // 올바른 괄호 문자열이면 카운트
         if(checkCorrectString(string)){
             answer++;
         }
