16d4163d1b0c54dd415b8021f806b993073825cb
Seob <78734659+mcar100@users.noreply.github.com>
2023-12-07 18:33:25 +0900

    [level 2] Title: 전화번호 목록, Time: 444.96 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 97.4 MB, 시간: 386.80 ms
+메모리: 99.1 MB, 시간: 444.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 18:4:31
+2023년 12월 4일 18:33:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,35 +1,38 @@
+// // 내 방식: 효율성 3,4 실패
+// // 11 110 123 1234 134 21 231 2321 233 ....
+// // 접두어면 항상 맨 앞이라는 의미
+// // 점점 길이가 길어지면 뒤에 것과 접두어로 매칭되지 않으니 패스
+
 // function solution(phone_book) {
-//     let answer = true;
 //     // 사전 순 정렬 
 //     phone_book.sort();
 //     for(let i=0; i<phone_book.length-1; i++){
 //         const fixed = phone_book[i];
-//         const rest = [...phone_book.slice(i+1)];
-//         const index = rest.findIndex(el=>el.includes(fixed));
-//         if(index!==-1){
-//             answer=false;
-//             break;
+//         for(let j=i+1; j<phone_book.length; j++){
+//             if(fixed.length>phone_book[j].length){
+//                 continue;
+//             }
+//             if(phone_book[j].startsWith(fixed)){
+//                 return false;
+//             }
 //         }
-//     }   
-    
-    
-//     return answer;
+//     }      
+//     return true;
 // }
 
+// // 상대방의 방식: 통과
 function solution(phoneBook) {
-const table = {};
+    const table = {};
 
-for (const number of phoneBook) {
-table[number] = true;
-};
+    for (const number of phoneBook) {
+        table[number] = true;
+    };
 
-for (const number of phoneBook) {
-for (let i = 1; i < number.length; i += 1) {
-const prefix = number.slice(0, i);
-if (table[prefix]) return false;
-
-};
-};
-
-return true;
+    for (const number of phoneBook) {
+        for (let i = 1; i < number.length; i += 1) {
+            const prefix = number.slice(0, i);
+            if (table[prefix]) return false;
+        };
+    };
+    return true;
 }
