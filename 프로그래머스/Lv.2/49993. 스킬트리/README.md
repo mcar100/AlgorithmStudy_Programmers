0e827f4ed038d147879cf4da2de3452143c5d771
Seob <78734659+mcar100@users.noreply.github.com>
2024-01-12 18:58:03 +0900

    [level Lv.2] Title: 스킬트리, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/49993. 스킬트리/README.md
+++ 프로그래머스/Lv.2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level Lv.2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#fnref1) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.32 ms
+메모리: 33.6 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 18:44:26
+2024년 1월 5일 18:58:1
 
 ### 문제 설명
 
--- 프로그래머스/Lv.2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/Lv.2/49993. 스킬트리/스킬트리.js
@@ -1,31 +1,21 @@
-function checkSkillTree(skill,skill_tree){
-    const skill_checkList = skill.split('').map(s=>[s,-1]);
-    
-    skill_checkList.forEach(skill=>{
-        skill[1] = skill_tree.indexOf(skill[0]);
-    });
-    
-    let cur_order = skill_checkList[0][1];
-    for(let j=1; j<skill_checkList.length; j++){
-        const next_order = skill_checkList[j][1];
-        if(cur_order>next_order&&next_order!==-1){
-            return false;
-        }
-        if(cur_order===-1&&next_order!==-1){
-            return false;
-        }
-        cur_order = next_order;
-    }
-    return true;
-}
-
 function solution(skill, skill_trees) {
-    let answer = 0;
-    for(let i=0; i<skill_trees.length; i++){
-        const isCorrect = checkSkillTree(skill, skill_trees[i]);
-        if(isCorrect){
-            answer++;
+    
+    function checkSkillTree(skill_tree){
+        const skill_checkList = skill.split('').map(s=>[s,skill_tree.indexOf(s)]);
+    
+        let cur_order = skill_checkList[0][1];
+        for(let j=1; j<skill_checkList.length; j++){
+            const next_order = skill_checkList[j][1];
+            if(cur_order>next_order&&next_order!==-1){
+                return false;
+            }
+            if(cur_order===-1&&next_order!==-1){
+                return false;
+            }
+            cur_order = next_order;
         }
+        return true;
     }
-    return answer;
+    
+    return skill_trees.filter(checkSkillTree).length;
 }
