190284b3e6732c9d00b86d1e505f25687fdd5fcf
Seob <78734659+mcar100@users.noreply.github.com>
2024-01-24 17:45:52 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,45 @@
+/* 연산식
+    1. x+n  -> y-n
+    ===============
+    2. x*2  -> y/2
+    3. x*3  -> y/3
+*/
+
+
+function solution(x, y, n) {
+    var answer = [];
+    const bfs = [{z:y,count:0}];
+    const operand = [3,2,n];
+    const operator = ['/','/','-'];
+    
+    if(x===y){
+        return 0;
+    }
+    
+    while(bfs.length!==0){
+        const {z,count} = bfs.shift(); 
+        if(z===x){
+            return count;
+        }
+        for(let i=0; i<operand.length; i++){
+            let newZ = z;
+            if(operator[i]==='/'){
+                newZ /= operand[i];
+            }
+            else if(operator[i]==='-'){
+                newZ -= operand[i];
+            }
+
+            if(newZ < x){
+                continue;
+            }
+            if(!Number.isInteger(newZ)){
+                continue;
+            }
+            bfs.push({z:newZ, count:count+1});
+        }
+                
+    }
+    return -1;
+
+}
