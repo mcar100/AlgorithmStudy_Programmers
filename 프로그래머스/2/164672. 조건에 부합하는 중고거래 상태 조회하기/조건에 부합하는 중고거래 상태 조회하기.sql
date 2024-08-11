9027872b48a7bd788f274d01a2741696a759d2cd
Seob <78734659+mcar100@users.noreply.github.com>
2024-08-12 08:51:11 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT board_id, writer_id, title, price, 
+    CASE
+        WHEN status = 'SALE' THEN '판매중'
+        WHEN status = 'RESERVED' THEN '예약중'
+        WHEN status = 'DONE' THEN '거래완료'
+    END AS status
+    FROM used_goods_board
+    WHERE created_date = '2022-10-05'
+    ORDER BY board_id DESC;
