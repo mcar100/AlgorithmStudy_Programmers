28c9788a3fb356772542c12d6d729fa931aa50f8
Seob <78734659+mcar100@users.noreply.github.com>
2024-08-12 09:29:09 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
 SELECT 
-    (select score from hr_grade where half_year = 1 AND emp_no=e.emp_no)+(select score from hr_grade where half_year = 2 AND emp_no=e.emp_no) score, 
+    SUM(g.score) score, 
     e.emp_no, e.emp_name, e.position, e.email   FROM hr_employees e, hr_grade g
     WHERE e.emp_no = g.emp_no
     GROUP BY e.emp_no
