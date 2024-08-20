7cef07780f8fc6f92c33afb2d78e8a3c071ccc7a
Seob <78734659+mcar100@users.noreply.github.com>
2024-08-20 09:11:10 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,6 +1,7 @@
 -- 코드를 작성해주세요
-SELECT DISTINCT dev.id, dev.email, dev.first_name, dev.last_name  
+SELECT dev.id, dev.email, dev.first_name, dev.last_name  
 FROM developers dev,
 (SELECT code FROM skillcodes WHERE name IN ('Python', 'C#')) sc
 WHERE dev.skill_code & sc.code> 0
+GROUP BY dev.id, dev.email, dev.first_name, dev.last_name  
 ORDER BY dev.id
